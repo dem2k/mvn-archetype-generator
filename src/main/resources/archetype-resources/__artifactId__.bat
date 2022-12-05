efb0c307a121d221a08fec9489d9fbcfd38e1f90
x004123 <dimitri.kleyn@fv.nrw.de>
2022-12-05 16:28:44 +0100

    create dummy properties at first run.

--- src/main/resources/archetype-resources/__artifactId__.bat
+++ src/main/resources/archetype-resources/__artifactId__.bat
@@ -1,4 +1,11 @@
 @echo off
+if not exist %~dp0config\properties.bat (
+	mkdir %~dp0config 1>nul 2>&1
+	echo @rem put your properties here. >> %~dp0config\properties.bat
+	echo warning: check your settings in %~dp0config\properties.bat before first run.
+	exit
+)
+call %~dp0config\properties.bat
 set classpath=%~dp0config;%~dp0target\classes
 for %%i in ("%~dp0target\dependency\*.jar") do call :addcp %%i
 java dem2k.AppMain %*
