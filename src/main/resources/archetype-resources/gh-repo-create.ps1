a221b8d569701625a8bd248b8e119d0fb8b8f3c9
x004123 <dimitri.kleyn@fv.nrw.de>
2022-12-05 16:08:32 +0100

    refactor git command.

--- src/main/resources/archetype-resources/gh-repo-create.ps1
+++ src/main/resources/archetype-resources/gh-repo-create.ps1
@@ -1,12 +1,12 @@
-$repo = Split-Path -Path . -Leaf
-$ans = read-host "About to create GitHub Repository [$repo]. Continue or type in new Name? "
-if ($ans -ne "") { $repo = $ans }
-if ((git remote -v | measure).count -gt 0) { 
+$repo = $pwd | Split-Path -Leaf
+$ansr = read-host "About to create GitHub Repository [$repo]. Continue or type in new Name? "
+if ($ansr -ne "") { $repo = $ansr }
+if ((git.exe remote -v | measure).count -gt 0) { 
     write-host "git remote already exists:"
-    git remote -v
+    git.exe remote -v
     write-host "`n enter will overwrite origin!"
     pause
 }
-git remote -v remove origin >$null 2>&1
+git.exe remote -v remove origin >$null 2>&1
 gh.exe repo create $repo --source=$pwd --public
 pause
