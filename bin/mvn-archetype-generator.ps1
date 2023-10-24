b18ba6f5f4da2477bd8e1d52a8e76ee46fe78402
x004123 <dimitri.kleyn@fv.nrw.de>
2023-10-24 14:33:28 +0200

    Create gitignore localy.

--- bin/mvn-archetype-generator.ps1
+++ bin/mvn-archetype-generator.ps1
@@ -13,10 +13,50 @@ Write-Host "Starting maven archetype plugin..."
 & mvn archetype:generate -DarchetypeGroupId="dem2k" -DarchetypeArtifactId="simple-archetype" -DinteractiveMode="false" -DgroupId="foobar" -DartifactId="$ProjectName" -Dversion="0.1.1-SNAPSHOT"
 
 cd $ProjectName
-wget.exe "https://gitignore.io/api/java,maven" -O .gitignore
+# wget.exe "https://gitignore.io/api/java,maven" -O .gitignore
 
 # === GitIgnore ===
 @'
+# Created by https://www.toptal.com/developers/gitignore/api/java,maven
+
+### Java ###
+# Compiled class file
+*.class
+
+# Log file
+*.log
+
+# Package Files
+*.jar
+*.war
+*.nar
+*.ear
+*.zip
+*.tar.gz
+*.rar
+
+# virtual machine crash logs
+hs_err_pid*
+replay_pid*
+
+### Maven ###
+/target
+pom.xml.tag
+pom.xml.releaseBackup
+pom.xml.versionsBackup
+pom.xml.next
+release.properties
+dependency-reduced-pom.xml
+buildNumber.properties
+.mvn/timing.properties
+.mvn/wrapper/maven-wrapper.jar
+
+# Eclipse generated files
+.project
+.classpath
+
+# End of https://www.toptal.com/developers/gitignore/api/java,maven
+
 /my-*
 /gh-*
 /git-*
@@ -31,3 +71,4 @@ $Answer = Read-Host "`n => Initialize Git-Flow? (Y/n) "
 if ($Answer -eq "y" -Or $Answer -eq "Y" -Or $Answer -eq "") {
         & "./git-flow-init.bat"
 }
+
