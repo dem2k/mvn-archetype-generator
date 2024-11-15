a3838bcfc48dac2b35da3c4e5c5616b5d715275c
x004123 <dimitri.kleyn@fv.nrw.de>
2024-11-15 13:00:30 +0100

    add git-flow version tag.

--- src/main/resources/archetype-resources/git-flow-init.bat
+++ src/main/resources/archetype-resources/git-flow-init.bat
@@ -3,4 +3,5 @@ git.exe init
 git.exe config gitflow.branch.master main                                                                                      
 git.exe config gitflow.branch.develop develop
 git.exe config gitflow.path.hooks .git/hooks
-git.exe flow init -f -d --feature="feature/" --bugfix="bugfix/" --release="release/" --hotfix="hotfix/" --support="support/" --tag=""
+git.exe flow init -f -d --feature="feature/" --bugfix="bugfix/" --release="release/" --hotfix="hotfix/" --support="support/" --tag="version/"
+
